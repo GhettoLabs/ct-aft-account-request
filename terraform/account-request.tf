d2ac5d8618cd50f3017bd549459d693265bd2bc8
james-knott <33873365+james-knott@users.noreply.github.com>
2023-04-23 12:19:07 -0500

    Update account-request.tf

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -1,23 +1,23 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-module "sandbox_account_01" {
+module "sandbox_account_02" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "sandbox01@ghettolabs.io"
-    AccountName  = "sandbox-account-01"
+    AccountEmail = "sandbox02@ghettolabs.io"
+    AccountName  = "sandbox-account-02"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "AFT-Management-OU"
+    # ManagedOrganizationalUnit = "AFT-Management-OU"
     # Syntax for nested OU
-    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "sandbox01@ghettolabs.io"
-    SSOUserFirstName = "Sandbox01"
+    ManagedOrganizationalUnit = "AFT-Management-OU (ou-naop-6yxklipy)"
+    SSOUserEmail     = "sandbox02@ghettolabs.io"
+    SSOUserFirstName = "Sandbox02"
     SSOUserLastName  = "Account"
   }
 
   account_tags = {
-    "ABC:Owner"       = "sandbox01@ghettolabs.io"
+    "ABC:Owner"       = "sandbox02@ghettolabs.io"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
