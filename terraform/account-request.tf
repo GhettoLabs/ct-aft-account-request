c91007e9a04874d624d2e77e7eec7a5a19e034ca
james-knott <33873365+james-knott@users.noreply.github.com>
2023-04-25 19:35:01 -0500

    Update account-request.tf

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -79,3 +79,42 @@ module "sandbox_account_02" {
 
   account_customizations_name = "sandbox-customizations"
 }
+
+module "sandbox_account_04" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail = "sandbox04@ghettolabs.io"
+    AccountName  = "sandbox-account-04"
+    # Syntax for top-level OU
+    # ManagedOrganizationalUnit = "AFT-Management-OU"
+    # Syntax for nested OU
+    ManagedOrganizationalUnit = "AFT-Management-OU (ou-naop-6yxklipy)"
+    SSOUserEmail     = "sandbox04@ghettolabs.io"
+    SSOUserFirstName = "Sandbox04"
+    SSOUserLastName  = "Account"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "sandbox04@ghettolabs.io"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "John Doe"
+    change_reason       = "testing the account vending process"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "sandbox-customizations"
+}
