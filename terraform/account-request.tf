31f4c8724b40a1bf8fb1908560550e54010521f8
James Knott <121402709+leaddevopsengineer@users.noreply.github.com>
2023-04-27 21:25:37 -0500

    UPDATED TO INCLUDE THE NEW PRODUCTION ACCOUNT
    UPDATED TO INCLUDE THE NEW PRODUCTION ACCOUNT

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -3,7 +3,7 @@
 #
 module "staging_account" {
   source = "./modules/aft-account-request"
-
+  
   control_tower_parameters = {
     AccountEmail = "staging@ghettolabs.io"
     AccountName  = "Staging"
@@ -39,7 +39,44 @@ module "staging_account" {
 
   account_customizations_name = "ghettolabs-customizations"
 }
+module "production_account" {
+  source = "./modules/aft-account-request"
+  
+  control_tower_parameters = {
+    AccountEmail = "production@ghettolabs.io"
+    AccountName  = "production"
+    # Syntax for top-level OU
+    # ManagedOrganizationalUnit = "AFT-Management-OU"
+    # Syntax for nested OU
+    ManagedOrganizationalUnit = "AFT-Management-OU (ou-mvw8-7azv2nsm)"
+    SSOUserEmail     = "production@ghettolabs.io"
+    SSOUserFirstName = "production"
+    SSOUserLastName  = "account"
+  }
 
+  account_tags = {
+    "ABC:Owner"       = "production@ghettolabs.io"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "James Knott"
+    change_reason       = "Adding production Account to Control Tower"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "ghettolabs-customizations"
+}
   
 #   module "sandbox_account_03" {
 #   source = "./modules/aft-account-request"
