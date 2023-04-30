d174878a00fc0dd006bd15c3fdaae46066aeba46
James Knott <121402709+leaddevopsengineer@users.noreply.github.com>
2023-04-30 14:05:45 -0500

    Added Development Account

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -39,6 +39,46 @@ module "staging_account" {
 
   account_customizations_name = "ghettolabs-customizations"
 }
+  
+module "development_account" {
+  source = "./modules/aft-account-request"
+  
+  control_tower_parameters = {
+    AccountEmail = "dev@ghettolabs.io"
+    AccountName  = "Development"
+    # Syntax for top-level OU
+    # ManagedOrganizationalUnit = "AFT-Management-OU"
+    # Syntax for nested OU
+    ManagedOrganizationalUnit = "AFT-Management-OU (ou-mvw8-7azv2nsm)"
+    SSOUserEmail     = "dev@ghettolabs.io"
+    SSOUserFirstName = "dev"
+    SSOUserLastName  = "account"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "dev@ghettolabs.io"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "James Knott"
+    change_reason       = "Adding Development Account to Control Tower"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "ghettolabs-customizations"
+}
+  
 module "production_account" {
   source = "./modules/aft-account-request"
   
@@ -78,80 +118,3 @@ module "production_account" {
   account_customizations_name = "ghettolabs-customizations"
 }
   
-#   module "sandbox_account_03" {
-#   source = "./modules/aft-account-request"
-
-#   control_tower_parameters = {
-#     AccountEmail = "sandbox03@ghettolabs.io"
-#     AccountName  = "sandbox-account-03"
-#     # Syntax for top-level OU
-#     # ManagedOrganizationalUnit = "AFT-Management-OU"
-#     # Syntax for nested OU
-#     ManagedOrganizationalUnit = "AFT-Management-OU (ou-naop-6yxklipy)"
-#     SSOUserEmail     = "sandbox03@ghettolabs.io"
-#     SSOUserFirstName = "Sandbox03"
-#     SSOUserLastName  = "Account"
-#   }
-
-#   account_tags = {
-#     "ABC:Owner"       = "sandbox03@ghettolabs.io"
-#     "ABC:Division"    = "ENT"
-#     "ABC:Environment" = "Dev"
-#     "ABC:CostCenter"  = "123456"
-#     "ABC:Vended"      = "true"
-#     "ABC:DivCode"     = "102"
-#     "ABC:BUCode"      = "ABC003"
-#     "ABC:Project"     = "123456"
-#   }
-
-#   change_management_parameters = {
-#     change_requested_by = "John Doe"
-#     change_reason       = "testing the account vending process"
-#   }
-
-#   custom_fields = {
-#     custom1 = "a"
-#     custom2 = "b"
-#   }
-
-#   account_customizations_name = "sandbox-customizations"
-# }
-
-# module "sandbox_account_04" {
-#   source = "./modules/aft-account-request"
-
-#   control_tower_parameters = {
-#     AccountEmail = "sandbox04@ghettolabs.io"
-#     AccountName  = "sandbox-account-04"
-#     # Syntax for top-level OU
-#     # ManagedOrganizationalUnit = "AFT-Management-OU"
-#     # Syntax for nested OU
-#     ManagedOrganizationalUnit = "AFT-Management-OU (ou-naop-6yxklipy)"
-#     SSOUserEmail     = "sandbox04@ghettolabs.io"
-#     SSOUserFirstName = "Sandbox04"
-#     SSOUserLastName  = "Account"
-#   }
-
-#   account_tags = {
-#     "ABC:Owner"       = "sandbox04@ghettolabs.io"
-#     "ABC:Division"    = "ENT"
-#     "ABC:Environment" = "Dev"
-#     "ABC:CostCenter"  = "123456"
-#     "ABC:Vended"      = "true"
-#     "ABC:DivCode"     = "102"
-#     "ABC:BUCode"      = "ABC003"
-#     "ABC:Project"     = "123456"
-#   }
-
-#   change_management_parameters = {
-#     change_requested_by = "John Doe"
-#     change_reason       = "testing the account vending process"
-#   }
-
-#   custom_fields = {
-#     custom1 = "a"
-#     custom2 = "b"
-#   }
-
-#   account_customizations_name = "sandbox-customizations"
-# }
