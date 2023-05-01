057907085a8798359f59326b49314c1faa7aa26f
James Knott <121402709+leaddevopsengineer@users.noreply.github.com>
2023-05-01 03:59:15 -0500

    Added Test Account

--- terraform/account-request.tf
+++ terraform/account-request.tf
@@ -40,6 +40,45 @@ module "staging_account" {
   account_customizations_name = "ghettolabs-customizations"
 }
   
+module "test_account" {
+  source = "./modules/aft-account-request"
+  
+  control_tower_parameters = {
+    AccountEmail = "test@ghettolabs.io"
+    AccountName  = "Test"
+    # Syntax for top-level OU
+    # ManagedOrganizationalUnit = "AFT-Management-OU"
+    # Syntax for nested OU
+    ManagedOrganizationalUnit = "AFT-Management-OU (ou-mvw8-7azv2nsm)"
+    SSOUserEmail     = "test@ghettolabs.io"
+    SSOUserFirstName = "test"
+    SSOUserLastName  = "account"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "test@ghettolabs.io"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "James Knott"
+    change_reason       = "Adding Test Account to Control Tower"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "ghettolabs-customizations"
+}
+  
 module "development_account" {
   source = "./modules/aft-account-request"
   
